8a4de2afbd1cb2f3fee326d1f3e83c392df37b7c
atreb_g <maurogiacomo.gb@gmail.com>
2021-11-01 19:20:21 +0100

    Initial commit

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 #main url per far funzionare il tutto
 #min 14
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #17,14
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
