ddd8bf6d9ba3fa9f3c83a081dcf2d7f62efb9db2
atreb_g <maurogiacomo.gb@gmail.com>
2021-12-16 12:50:48 +0100

    Initial commit

--- website/auth.py
+++ website/auth.py
@@ -7,13 +7,26 @@ auth = Blueprint('auth', __name__)
 def login():
     return "<p> Login </p>"
 
+       # user = User.query.filter_by(email=email).first()
+        #if user:
+         #   if check_password_hash(user.password, password):
+          #      flash('Logged in successfully!', category='success')
+           #     login_user(user, remember=True)
+            #    return redirect(url_for('views.home'))
+            #else:
+             #   flash('Incorrect password, try again.', category='error')
+        #else:
+     #       flash('Email does not exist.', category='error') """
+    return render_template("login.html")
+
+
 
 @auth.route('/logout')
 def logout():
     return "<p> Logout </p>"
 
 
-@auth.route('/sing-up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     return "<p> Sign Up </p>"
 
