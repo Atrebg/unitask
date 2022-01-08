e4b480a51ed77e5528894a784bceae311d9ffb4e
atreb_g <maurogiacomo.gb@gmail.com>
2022-01-08 20:04:54 +0100

    sistemato views mancano le maps

--- website/views.py
+++ website/views.py
@@ -233,11 +233,33 @@ def updateaccount():
        #manca il render template capire cosa serve
     return render_template(user = current_user, form=form)
 
-
-
 @views.route('/resetdb')
 def resetdb():
     db.drop_all()
     db.create_all()
     riempidb()
     return redirect(url_for('auth.logout'))
+
+
+@views.route('/maps')
+@login_required
+def maps():
+    locations = [
+    {"title": "Death Valley National Park", "address1": "California", "address2": "United States",
+     "coords": {"lat": 36.4617, "lng": -116.8668}, "placeId": "ChIJR4qudndLx4ARVLDye3zwycw"},
+    {"title": "Yosemite National Park", "address1": "California", "address2": "USA",
+     "coords": {"lat": 37.7487, "lng": -119.5873}, "placeId": "ChIJxeyK9Z3wloAR_gOA7SycJC0"},
+    {"title": "Joshua Tree National Park", "address1": "California", "address2": "USA",
+     "coords": {"lat": 33.9157, "lng": -115.8807}, "placeId": "ChIJe6hluYWP2oAR4p3rOqftdxk"},
+    {"title": "Lassen Volcanic National Park", "address1": "California", "address2": "USA",
+     "coords": {"lat": 40.4377, "lng": -121.5338}, "placeId": "ChIJvzhBwQdWnYARQmdmeqfYNI8"},
+    {"title": "Sequoia National Park", "address1": "California", "address2": "USA",
+     "coords": {"lat": 36.491, "lng": -118.8253}, "placeId": "ChIJeWUZLX37v4ARZPQen_nfCkQ"},
+    {"title": "Pinnacles National Park", "address1": "California", "address2": "USA",
+     "coords": {"lat": 36.4938, "lng": -121.1465}, "placeId": "ChIJn93OiYBDkoAR7kSomO77gps"},
+    {"title": "Redwood National and State Parks", "address1": "California", "address2": "USA",
+     "coords": {"lat": 41.3025, "lng": -124.0471}, "placeId": "ChIJpX9B9TZm0FQRjl87lWYyzzY"},
+    {"title": "Latina", "address1": "Via Palestro", "address2": "ITA",
+     "coords": {"lat": 41.4662056, "lng": 12.8971524}, "placeId": "ChIJPSJsk4ALJRMR1Rctc49Mwzw"} ]
+
+    return render_template("maps.html", user=current_user)
